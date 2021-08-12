18d80e7e3c89318edd0ca365cff7045ed07d6add
Sokhorn Khy <sokhornkhy88@gmail.com>
2021-08-12 13:12:21 +0700

    add new comment

--- pushAndBuild.sh
+++ pushAndBuild.sh
@@ -4,4 +4,4 @@ git add .
 git commit -m "$commit"
 git push origin master
 #build apk file
-sh /home/sokhorn/sokhorn/selfLearn/python/tensorflow/version1/buildapi.sh
+sh /home/sokhorn/sokhorn/selfLearn/python/tensorflow/version1/buildApk.sh
